c6c7890dae9e9266c63a7dbfd547511f51ea491c
jredmundson <jredmundson@ad.engr.wisc.edu>
2025-03-13 17:27:51 -0500

    initial

---
+++ Verilog/ethernet/eth_top.sv
@@ -0,0 +1,11 @@
+module eth_top (
+    input  logic clk,
+    input  logic rst_n,
+    input  logic [31:0] data_in,
+    input  logic        dval_in,
+    output logic [31:0] data_out,
+    output logic        dval_out
+);
+
+
+endmodule
