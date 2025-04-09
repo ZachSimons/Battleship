60875a5a7fe803a44013248b362c5a40ee0ce1ec
aekstrand <aekstrand@wisc.edu>
2025-04-09 17:08:47 -0500

    Finished optimizing algorithm

--- C/battleshipP1_cmd.c
+++ C/battleshipP1_cmd.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <time.h>
 #include <stdlib.h>
+#include <string.h>
 
 #define BOARD_SIZE 10
 #define NUM_SQUARES (BOARD_SIZE * BOARD_SIZE)
@@ -10,6 +11,7 @@
 #define EMPTY 0
 #define MISS  1
 #define HIT   2
+#define SUNK  3
 
 #define GET_BOARD_SEL(x) (x >> 31)
 #define GET_SHIP_POS(x) ((x & 0x7f000000) >> 24)
@@ -32,84 +34,110 @@
 int my_board[NUM_SQUARES];
 char target_board[NUM_SQUARES];
 int enemy_board[NUM_SQUARES];
+int enemy_positions[MAX_SHIPS];
 char ship_sizes[MAX_SHIPS] = {2, 3, 3, 4, 5};
+int sunk[MAX_SHIPS] = {-1, -1, -1, -1, -1};
 char ship_letter[MAX_SHIPS] = {'d', 's', 'c', 'b', 'a'};
 
 // Accelerator data
 char possible_positions[MAX_SHIPS][TOTAL_SHIP_POSITIONS];
-char invalid_combos[MAX_SHIPS][TOTAL_SHIP_POSITIONS][MAX_SHIPS-1][TOTAL_SHIP_POSITIONS];
+char invalid_combos[MAX_SHIPS][TOTAL_SHIP_POSITIONS][MAX_SHIPS][TOTAL_SHIP_POSITIONS];
 int hit_counts[NUM_SQUARES];
 #define ACCELERATOR_COUNT 8000
 
-void clear_board(int board) {
-    for(int i = 0; i < NUM_SQUARES; i++) {
-        if(board) {
-            my_board[i] = 0;
-        } else {
-            enemy_board[i] = 0;
-        }
-    }
-}
-
-int place_ship(int board, int square, int v, int type) {
+int place_enemy_ship(int square, int v, int type) {
     int size = ship_sizes[type];
     int inc = v ? 10 : 1;
     if(v) {
-        if(square + inc*size > 99) {
+        if(square + inc*(size-1) > 99) {
             return 0;
         }
     } else {
-        if ((square%10) + inc*size > 10) {
+        if ((square%10) + inc*(size-1) > 9) {
             return 0;
         }
     }
     for(int i = 0; i < size; i++) {
-        if(board) {
-            if(GET_E_BIT(my_board[square + inc*i])) {
-                return 0;
-            }
-        } else {
-            if(GET_E_BIT(enemy_board[square + inc*i])) {
-                return 0;
-            }
+        if(GET_E_BIT(enemy_board[square + inc*i])) {
+            return 0;
         }
     }
     for(int i = 0; i < size; i++) {
-        if(board) {
-            my_board[square + inc*i] = SET_TYPE(type) | SET_E_BIT(1) | SET_V_BIT(v) | SET_SEG(i);
-        } else {
-            enemy_board[square + inc*i] = SET_TYPE(type) | SET_E_BIT(1) | SET_V_BIT(v) | SET_SEG(i);
+        enemy_board[square + inc*i] = SET_TYPE(type) | SET_E_BIT(1) | SET_V_BIT(v) | SET_SEG(i);
+    }
+    enemy_positions[type] = square + 100*v;
+    return 1;
+}
+
+int place_ship(int square, int v, int type) {
+    int size = ship_sizes[type];
+    int inc = v ? 10 : 1;
+    if(v) {
+        if(square + inc*(size-1) > 99) {
+            return 0;
+        }
+    } else {
+        if ((square%10) + inc*(size-1) > 9) {
+            return 0;
+        }
+    }
+    for(int i = 0; i < size; i++) {
+        if(GET_E_BIT(my_board[square + inc*i])) {
+            return 0;
+        }
+    }
+    for(int i = 0; i < size; i++) {
+        my_board[square + inc*i] = SET_TYPE(type) | SET_E_BIT(1) | SET_V_BIT(v) | SET_SEG(i);
+    }
+    return 1;
+}
+
+int check_win() {
+    for(int i = 0; i < MAX_SHIPS; i++) {
+        if(sunk[i] == -1) {
+            return 0;
         }
     }
     return 1;
 }
 
-int check_win(int board) {
+int check_lose() {
     for(int i = 0; i < NUM_SQUARES; i++) {
-        if(board) {
-            if(GET_E_BIT(my_board[i]) && !GET_M_BIT(my_board[i])) {
-                return 0;
-            }
-        } else {
-            if(GET_E_BIT(enemy_board[i]) && !GET_M_BIT(enemy_board[i])) {
-                return 0;
-            }
+        if(GET_E_BIT(my_board[i]) && !GET_M_BIT(my_board[i])) {
+            return 0;
         }
     }
     return 1;
 }
 
-void print_board(int board, int turn, int target) {
+void print_target_board(int AI) {
+    for(int i = 0; i < BOARD_SIZE; i++) {
+        for(int j = 0; j < BOARD_SIZE; j++) {
+            if(i*BOARD_SIZE + j == AI) {
+                printf("|AI|");
+            } else if(target_board[i*BOARD_SIZE + j] == EMPTY) {
+                printf("|..|");
+            } else if(target_board[i*BOARD_SIZE + j] == MISS) {
+                printf("|oo|");
+            } else if(target_board[i*BOARD_SIZE + j] == HIT) {
+                printf("|.x|");
+            } else {
+                printf("|XX|");
+            }
+        }
+        printf("\n");
+    }
+}
+
+void print_board(int board) {
     if(board) {
-        printf("______________Enemy Board______________\n");
-    } else {
         printf("________________My Board_______________\n");
+    } else {
+        printf("______________Enemy Board______________\n");
     }
     for(int i = 0; i < BOARD_SIZE; i++) {
         for(int j = 0; j < BOARD_SIZE; j++) {
-            if(board == turn && target == i*BOARD_SIZE + j) {
-                printf("|AI|");
-            } else if(board) {
+            if(board) {
                 if(GET_E_BIT(my_board[i*10 + j])) {
                     int type = GET_TYPE(my_board[i*10 + j]);
                     printf("|%c", ship_letter[type]);
@@ -146,7 +174,7 @@ void print_board(int board, int turn, int target) {
         }
         printf("\n");
     }
-    printf("_______________________________________\n");
+    printf("---------------------------------------\n");
 }
 
 void clear_possible_positions() {
@@ -191,16 +219,16 @@ int check_valid_position(int ship, int square, int v) {
     int size = ship_sizes[ship];
     int inc = v ? 10 : 1;
     if(v) {
-        if(square + inc*size > 99) {
+        if(square + inc*(size-1) > 99) {
             return 0;
         }
     } else {
-        if (((square+1)%10) + inc*size > 10) {
+        if ((square%10) + inc*(size-1) > 9) {
             return 0;
         }
     }
     for(int i = 0; i < size; i++) {
-        if(target_board[square + inc*i] == MISS) {
+        if(target_board[square + inc*i] == MISS || target_board[square + inc*i] == SUNK) {
             return 0;
         }
     }
@@ -250,10 +278,14 @@ int run_accelerator() {
     clear_accelerator_data();
     // Get all valid positions
     for(int ship = 0; ship < MAX_SHIPS; ship++) {
-        for(int v = 0; v < 2; v++) {
-            for(int square = 0; square < NUM_SQUARES; square++) {
-                if(check_valid_position(ship, square, v)) {
-                    possible_positions[ship][v*100 + square] = 1;
+        if(sunk[ship] != -1) {
+            possible_positions[ship][sunk[ship]] = 1;
+        } else {
+            for(int v = 0; v < 2; v++) {
+                for(int square = 0; square < NUM_SQUARES; square++) {
+                    if(check_valid_position(ship, square, v)) {
+                        possible_positions[ship][v*100 + square] = 1;
+                    }
                 }
             }
         }
@@ -279,31 +311,33 @@ int run_accelerator() {
         unsigned char ship_configuration[5];
         // Generate configuration
         for(int ship = 0; ship < MAX_SHIPS; ship++) {
-            while(1) {
-                ship_configuration[ship] = rand() % 200;
-                if(possible_positions[ship][ship_configuration[ship]]) {
-                    int valid = 1;
-                    for(int lower = 0; lower < ship; lower++) {
-                        if(!invalid_combos[lower][ship_configuration[lower]][ship][ship_configuration[ship]]) {
-                            valid = 0;
-                            break;
-                        }
+            ship_configuration[ship] = rand() % 200;
+            if(possible_positions[ship][ship_configuration[ship]]) {
+                int valid = 1;
+                for(int lower = 0; lower < ship; lower++) {
+                    if(!invalid_combos[lower][ship_configuration[lower]][ship][ship_configuration[ship]]) {
+                        ship = 0;
+                        break;
                     }
-                    if(valid) break;
                 }
+            } else {
+                ship--;
             }
         }
         // Check if configuration is legal (ACCELERATOR)
         if(!check_valid_configuration(ship_configuration)) {
+            accel_cnt--;
             continue;
         }
         // Mark positions
         for(int ship = 0; ship < MAX_SHIPS; ship++) {
-            int size = ship_sizes[ship];
-            int pos = ship_configuration[ship] - 100;
-            int inc = ship_configuration[ship] >= 100 ? 10 : 1;
+            char size = ship_sizes[ship];
+            unsigned char pos = ship_configuration[ship] % 100;
+            unsigned char inc = ship_configuration[ship] >= 100 ? 10 : 1;
             for(int i = 0; i < size; i++) {
-                hit_counts[pos + inc*i]++;
+                if(target_board[pos + inc*i] == EMPTY) {
+                    hit_counts[pos + inc*i]++;
+                }
             }
         }
     }
@@ -321,39 +355,58 @@ int run_accelerator() {
     return target;
 }
 
+void check_sinks() {
+    for(int i = 0; i < MAX_SHIPS; i++) {
+        if(sunk[i] == -1) {
+            int isSunk = 1;
+            int inc = enemy_positions[i] > 99 ? 10 : 1;
+            int pos = enemy_positions[i] % 100;
+            for(int j = 0; j < ship_sizes[i]; j++) {
+                if(!GET_M_BIT(enemy_board[pos + inc*j])) {
+                    isSunk = 0;
+                }
+            }
+            if(isSunk) {
+                sunk[i] = enemy_positions[i];
+                for(int j = 0; j < ship_sizes[i]; j++) {
+                    target_board[pos + j*inc] = SUNK;
+                }
+            }
+        }
+    }
+}
+
 int main() {
     srand(time(NULL));
     for(int i = 0; i < 10; i++) {
-        while(!place_ship(i%2, rand() % 100, rand()%2, i/2)) {
-            // Do nothing
+        if(i%2) {
+            while(!place_ship(rand() % 100, rand()%2, i/2)) {
+                // Do nothing
+            }
+        } else {
+            while(!place_enemy_ship(rand() % 100, rand()%2, i/2)) {
+                // Do nothing
+            }
         }
     }
-    int turn = 1;
     int target;
     int guess;
     while(1) {
         target = run_accelerator();
-        print_board(0, !turn, target);
-        print_board(1, !turn, target);
+        print_board(0);
+        print_target_board(target);
+        print_board(1);
         printf("AI guess: %d\n", target);
         scanf("%d", &guess);
-        if(turn) {
-            enemy_board[guess] |= SET_M_BIT(1);
-            if(GET_E_BIT(enemy_board[guess])) {
-                target_board[guess] = MISS;
-            } else {
-                target_board[guess] = HIT;
-            }
+        enemy_board[guess] |= SET_M_BIT(1);
+        if(GET_E_BIT(enemy_board[guess])) {
+            target_board[guess] = HIT;
         } else {
-            my_board[guess] |= SET_M_BIT(1);
+            target_board[guess] = MISS;
         }
-        turn = (turn+1)%2;
-        if(check_win(turn)) {
-            if(turn) {
-                printf("Enemy wins!\n");
-            } else {
-                printf("You win!\n");
-            }
+        check_sinks();
+        if(check_win()) {
+            printf("You win!\n");
             break;
         }
     }
